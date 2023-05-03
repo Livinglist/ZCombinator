c5771e1606a79213ee81cb90b13e23c9f25105b8
Livinglist <georgefung78@live.com>
2023-05-03 00:26:52 -0700

    add widget.

---
+++ StoryWidget/StoryWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StoryWidgetBundle.swift
+//  StoryWidget
+//
+//  Created by Jiaqi Feng on 5/3/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StoryWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StoryWidget()
+    }
+}
