4e6b7b1ef71baa9a79203dc73405628db9d60b2b
Livinglist <georgefung78@live.com>
2023-05-07 18:27:19 -0700

    update comment fetching logic

--- Shared/Models/ItemStore.swift
+++ Shared/Models/ItemStore.swift
@@ -2,22 +2,56 @@ import Foundation
 import SwiftUI
 import HackerNewsKit
 
-extension ItemView {
-    @MainActor
-    class ItemStore : ObservableObject {
-        @Published var kids = [Comment]()
-        @Published var status: Status = .idle
-        @Published var item: (any Item)?
-        
-        func loadKids() async {
-            if let kids = self.item?.kids {
-                self.status = .loading
+@MainActor
+class ItemStore : ObservableObject {
+    @Published var kids = [Comment]()
+    @Published var status: Status = .idle
+    @Published var item: (any Item)?
+    @Published var loadingItem: Int?
+    @Published var loadedItems: Set<Int> = Set<Int>()
+    
+    func loadKids(of cmt: Comment) async {
+        guard let parentIndex = kids.firstIndex(of: cmt) else { return }
+        if let kids = cmt.kids, let level = cmt.level {
+            self.loadingItem = cmt.id
+            
+            var comments = [Comment]()
+            
+            await StoriesRepository.shared.fetchComments(ids: kids) { comment in
+                comments.append(comment.copyWith(level: level + 1))
+            }
+            
+            withAnimation {
+                self.kids.insert(contentsOf: comments, at: parentIndex + 1)
+                self.loadingItem = nil
+                self.loadedItems.insert(cmt.id)
+            }
+        }
+    }
+    
+    func refresh() async -> Void {
+        if let id = self.item?.id {
+            withAnimation {
+                self.loadingItem = nil
+                self.loadedItems = Set<Int>()
+                self.kids.removeAll()
+            }
+            self.status = .loading
+            
+            let item = await StoriesRepository.shared.fetchItem(id)
+            
+            if let item = item, let kids = item.kids {
+                print(kids)
+                self.item = item
                 
                 var comments = [Comment]()
                 
                 await StoriesRepository.shared.fetchComments(ids: kids) { comment in
-                    comments.append(comment)
+                    print(comment)
+                    comments.append(comment.copyWith(level: 0))
                 }
+                print(comments)
+                
                 
                 withAnimation {
                     self.kids.append(contentsOf: comments)
@@ -26,22 +60,6 @@ extension ItemView {
             } else {
                 withAnimation {
                     self.status = .loaded
-                }
-            }
-        }
-        
-        func refresh() async -> Void {
-            if let id = self.item?.id {
-                withAnimation {
-                    self.kids.removeAll()
-                }
-                self.status = .loading
-
-                let item = await StoriesRepository.shared.fetchItem(id)
-                
-                if let item = item {
-                    self.item = item
-                    await loadKids()
                 }
             }
         }
