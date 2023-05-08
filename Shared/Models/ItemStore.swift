e49af6bed48ddd61d1d95fe59ce21fc6fe1776b1
Livinglist <georgefung78@live.com>
2023-05-07 18:28:34 -0700

    reorder.

--- Shared/Models/ItemStore.swift
+++ Shared/Models/ItemStore.swift
@@ -2,66 +2,69 @@ import Foundation
 import SwiftUI
 import HackerNewsKit
 
-@MainActor
-class ItemStore : ObservableObject {
-    @Published var kids = [Comment]()
-    @Published var status: Status = .idle
-    @Published var item: (any Item)?
-    @Published var loadingItem: Int?
-    @Published var loadedItems: Set<Int> = Set<Int>()
-    
-    func loadKids(of cmt: Comment) async {
-        guard let parentIndex = kids.firstIndex(of: cmt) else { return }
-        if let kids = cmt.kids, let level = cmt.level {
-            self.loadingItem = cmt.id
-            
-            var comments = [Comment]()
-            
-            await StoriesRepository.shared.fetchComments(ids: kids) { comment in
-                comments.append(comment.copyWith(level: level + 1))
-            }
-            
-            withAnimation {
-                self.kids.insert(contentsOf: comments, at: parentIndex + 1)
-                self.loadingItem = nil
-                self.loadedItems.insert(cmt.id)
-            }
-        }
-    }
-    
-    func refresh() async -> Void {
-        if let id = self.item?.id {
-            withAnimation {
-                self.loadingItem = nil
-                self.loadedItems = Set<Int>()
-                self.kids.removeAll()
-            }
-            self.status = .loading
-            
-            let item = await StoriesRepository.shared.fetchItem(id)
-            
-            if let item = item, let kids = item.kids {
-                print(kids)
-                self.item = item
+extension ItemView {
+    @MainActor
+    class ItemStore : ObservableObject {
+        @Published var kids = [Comment]()
+        @Published var status: Status = .idle
+        @Published var item: (any Item)?
+        @Published var loadingItem: Int?
+        @Published var loadedItems: Set<Int> = Set<Int>()
+        
+        func loadKids(of cmt: Comment) async {
+            guard let parentIndex = kids.firstIndex(of: cmt) else { return }
+            if let kids = cmt.kids, let level = cmt.level {
+                self.loadingItem = cmt.id
                 
                 var comments = [Comment]()
                 
                 await StoriesRepository.shared.fetchComments(ids: kids) { comment in
-                    print(comment)
-                    comments.append(comment.copyWith(level: 0))
+                    comments.append(comment.copyWith(level: level + 1))
                 }
-                print(comments)
-                
                 
                 withAnimation {
-                    self.kids.append(contentsOf: comments)
-                    self.status = .loaded
+                    self.kids.insert(contentsOf: comments, at: parentIndex + 1)
+                    self.loadingItem = nil
+                    self.loadedItems.insert(cmt.id)
                 }
-            } else {
+            }
+        }
+        
+        func refresh() async -> Void {
+            if let id = self.item?.id {
                 withAnimation {
-                    self.status = .loaded
+                    self.loadingItem = nil
+                    self.loadedItems = Set<Int>()
+                    self.kids.removeAll()
+                }
+                self.status = .loading
+                
+                let item = await StoriesRepository.shared.fetchItem(id)
+                
+                if let item = item, let kids = item.kids {
+                    print(kids)
+                    self.item = item
+                    
+                    var comments = [Comment]()
+                    
+                    await StoriesRepository.shared.fetchComments(ids: kids) { comment in
+                        print(comment)
+                        comments.append(comment.copyWith(level: 0))
+                    }
+                    print(comments)
+                    
+                    
+                    withAnimation {
+                        self.kids.append(contentsOf: comments)
+                        self.status = .loaded
+                    }
+                } else {
+                    withAnimation {
+                        self.status = .loaded
+                    }
                 }
             }
         }
     }
+
 }
--- Shared/Views/Widgets/CommentTile.swift
+++ Shared/Views/Widgets/CommentTile.swift
@@ -3,174 +3,78 @@ import SwiftUI
 import WebKit
 import HackerNewsKit
 
-struct CommentTile: View {
-    @EnvironmentObject var auth: Authentication
-    @ObservedObject var itemStore: ItemStore
-    
-    @State private var isCollapsed = Bool()
-    @State private var showHNSheet = Bool()
-    @State private var showUrlSheet = Bool()
-    @State private var showReplySheet = Bool()
-    @State private var showFlagDialog = Bool()
-    @State private var showFlagToast = Bool()
-    @State private var showUpvoteToast = Bool()
-    @State private var showDownvoteToast = Bool()
-    @State private var showReplyToast = Bool()
-    @State private var showFavoriteToast = Bool()
-    @State private var showUnfavoriteToast = Bool()
-    private static var handledUrl: URL? = nil
-    
-    let level: Int
-    let comment: Comment
-    let settings = Settings.shared
-    let onLoadMore: () -> Void
-    
-    init(comment: Comment, itemStore: ItemStore, onLoadMore: @escaping () -> Void) {
-        self.level = comment.level ?? 0
-        self.comment = comment
-        self.onLoadMore = onLoadMore
-        self.itemStore = itemStore
-    }
-    
-    var body: some View {
-        mainView
-            .if(level > 0) { view -> AnyView in
-                var wrappedView = AnyView(view)
-                for i in (1...level).reversed() {
-                    wrappedView = AnyView(
-                        wrappedView
+extension ItemView {
+    struct CommentTile: View {
+        @EnvironmentObject var auth: Authentication
+        @ObservedObject var itemStore: ItemStore
+        
+        @State private var isCollapsed = Bool()
+        @State private var showHNSheet = Bool()
+        @State private var showUrlSheet = Bool()
+        @State private var showReplySheet = Bool()
+        @State private var showFlagDialog = Bool()
+        @State private var showFlagToast = Bool()
+        @State private var showUpvoteToast = Bool()
+        @State private var showDownvoteToast = Bool()
+        @State private var showReplyToast = Bool()
+        @State private var showFavoriteToast = Bool()
+        @State private var showUnfavoriteToast = Bool()
+        private static var handledUrl: URL? = nil
+        
+        let level: Int
+        let comment: Comment
+        let settings = Settings.shared
+        let onLoadMore: () -> Void
+        
+        init(comment: Comment, itemStore: ItemStore, onLoadMore: @escaping () -> Void) {
+            self.level = comment.level ?? 0
+            self.comment = comment
+            self.onLoadMore = onLoadMore
+            self.itemStore = itemStore
+        }
+        
+        var body: some View {
+            mainView
+                .if(level > 0) { view -> AnyView in
+                    var wrappedView = AnyView(view)
+                    for i in (1...level).reversed() {
+                        wrappedView = AnyView(
+                            wrappedView
                             
-                            .overlay(Rectangle().frame(width: 1, height: nil, alignment: .leading)
-                                .foregroundColor(getColor(level: i)), alignment: .leading)
-                            .padding(.leading, 6)
-                        
-                    )
-                }
-                
-                return AnyView(wrappedView)
-            }
-            .sheet(isPresented: $showHNSheet) {
-                if let url = URL(string: comment.itemUrl) {
-                    SafariView(url: url)
-                }
-            }
-            .sheet(isPresented: $showUrlSheet) {
-                if let url = Self.handledUrl {
-                    SafariView(url: url)
-                }
-            }
-            .sheet(isPresented: $showReplySheet) {
-                ReplyView(showReplyToast: $showReplyToast, replyingTo: comment)
-            }
-            .confirmationDialog("Are you sure?", isPresented: $showFlagDialog) {
-                Button("Flag", role: .destructive) {
-                    onFlagTap()
-                }
-            } message: {
-                Text("Flag the post by \(comment.by.orEmpty)?")
-            }
-    }
-    
-    @ViewBuilder
-    var menu: some View {
-        Menu {
-            UpvoteButton(id: comment.id, showUpvoteToast: $showUpvoteToast)
-            DownvoteButton(id: comment.id, showDownvoteToast: $showDownvoteToast)
-            FavButton(id: comment.id, showUnfavoriteToast: $showUnfavoriteToast, showFavoriteToast: $showFavoriteToast)
-            PinButton(id: comment.id)
-            Button {
-                showReplySheet = true
-            } label: {
-                Label("Reply", systemImage: "plus.message")
-            }
-            .disabled(!auth.loggedIn)
-            Divider()
-            FlagButton(id: comment.id, showFlagDialog: $showFlagDialog)
-            Divider()
-            ShareMenu(item: comment)
-            Button {
-                showHNSheet = true
-            } label: {
-                Label("View on Hacker News", systemImage: "safari")
-            }
-        } label: {
-            Label("", systemImage: "ellipsis")
-                .foregroundColor(.orange)
-        }
-    }
-    
-    @ViewBuilder
-    var textView: some View {
-        if comment.text.isNotNullOrEmpty {
-            Text(comment.text.orEmpty.markdowned)
-                .font(.system(size: 16))
-                .textSelection(.enabled)
-                .fixedSize(horizontal: false, vertical: true)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 4)
-        } else {
-            Text("deleted")
-                .font(.footnote)
-                .foregroundColor(.gray)
-                .padding(.top, 6)
-        }
-    }
-    
-    @ViewBuilder
-    var mainView: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            VStack(spacing: 0) {
-                nameRow.padding(.bottom, 4)
-                if isCollapsed {
-                    Button {
-                        HapticFeedbackService.shared.ultralight()
-                        withAnimation {
-                            isCollapsed.toggle()
-                        }
-                    } label: {
-                        Text("Collapsed")
-                            .font(.footnote.weight(.bold))
-                            .foregroundColor(getColor(level: level))
+                                .overlay(Rectangle().frame(width: 1, height: nil, alignment: .leading)
+                                    .foregroundColor(getColor(level: i)), alignment: .leading)
+                                .padding(.leading, 6)
+                            
+                        )
                     }
                     
-                } else {
-                    textView
-                        .toast(isPresenting: $showFlagToast) {
-                            AlertToast(type: .regular, title: "Flagged")
-                        }
-                        .toast(isPresenting: $showUpvoteToast) {
-                            AlertToast(type: .regular, title: "Upvoted")
-                        }
-                        .toast(isPresenting: $showDownvoteToast) {
-                            AlertToast(type: .regular, title: "Downvoted")
-                        }
-                        .toast(isPresenting: $showReplyToast) {
-                            AlertToast(type: .regular, title: "Replied")
-                        }
-                        .toast(isPresenting: $showFavoriteToast) {
-                            AlertToast(type: .regular, title: "Added")
-                        }
+                    return AnyView(wrappedView)
                 }
-                if itemStore.loadingItem == comment.id {
-                    LoadingIndicator().padding(.top, 16).padding(.bottom, 8)
-                } else if itemStore.loadedItems.contains(comment.id) == false && isCollapsed == false && comment.kids.isNotNullOrEmpty {
-                    Button {
-                        HapticFeedbackService.shared.light()
-                        
-                        onLoadMore()
-                    } label: {
-                        Text("Load \(comment.kids.countOrZero) \(comment.kids.isMoreThanOne ? "replies" : "reply")")
-                            .font(.footnote.weight(.bold))
-                            .foregroundColor(getColor(level: level))
+                .sheet(isPresented: $showHNSheet) {
+                    if let url = URL(string: comment.itemUrl) {
+                        SafariView(url: url)
                     }
-                    .buttonStyle(.bordered)
-                    .buttonBorderShape(.capsule)
-                    .padding(.top, 6)
                 }
-            }
-            .padding(EdgeInsets(top: 6, leading: 0, bottom: 0, trailing: 0))
-            .background(Color(UIColor.systemBackground))
-            .contextMenu {
+                .sheet(isPresented: $showUrlSheet) {
+                    if let url = Self.handledUrl {
+                        SafariView(url: url)
+                    }
+                }
+                .sheet(isPresented: $showReplySheet) {
+                    ReplyView(showReplyToast: $showReplyToast, replyingTo: comment)
+                }
+                .confirmationDialog("Are you sure?", isPresented: $showFlagDialog) {
+                    Button("Flag", role: .destructive) {
+                        onFlagTap()
+                    }
+                } message: {
+                    Text("Flag the post by \(comment.by.orEmpty)?")
+                }
+        }
+        
+        @ViewBuilder
+        var menu: some View {
+            Menu {
                 UpvoteButton(id: comment.id, showUpvoteToast: $showUpvoteToast)
                 DownvoteButton(id: comment.id, showDownvoteToast: $showDownvoteToast)
                 FavButton(id: comment.id, showUnfavoriteToast: $showUnfavoriteToast, showFavoriteToast: $showFavoriteToast)
@@ -190,52 +94,150 @@ struct CommentTile: View {
                 } label: {
                     Label("View on Hacker News", systemImage: "safari")
                 }
+            } label: {
+                Label("", systemImage: "ellipsis")
+                    .foregroundColor(.orange)
             }
-            .onTapGesture {
-                HapticFeedbackService.shared.ultralight()
-                withAnimation {
-                    isCollapsed.toggle()
-                }
-            }
-            Spacer()
         }
-        .frame(alignment: .leading)
-        .padding(.leading, 6)
-    }
-    
-    @ViewBuilder
-    var nameRow: some View {
-        HStack {
-            Text(comment.by.orEmpty)
-                .borderedFootnote()
-                .foregroundColor(getColor(level: level))
-            if let karma = comment.score {
-                Text("\(karma) karma")
-                    .borderedFootnote()
-                    .foregroundColor(getColor(level: level))
-            }
-            if let descendants = comment.descendants {
-                Text("\(descendants) comment\(descendants <= 1 ? "" : "s")")
-                    .borderedFootnote()
-                    .foregroundColor(getColor(level: level))
-            }
-            Spacer()
-            Text(comment.timeAgo)
-                .borderedFootnote()
-                .foregroundColor(getColor(level: level))
-                .padding(.trailing, 2)
-        }
-    }
-    
-    private func onFlagTap() {
-        Task {
-            let res = await AuthRepository.shared.flag(comment.id)
-            
-            if res {
-                showFlagToast = true
-                HapticFeedbackService.shared.success()
+        
+        @ViewBuilder
+        var textView: some View {
+            if comment.text.isNotNullOrEmpty {
+                Text(comment.text.orEmpty.markdowned)
+                    .font(.system(size: 16))
+                    .textSelection(.enabled)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 4)
             } else {
-                HapticFeedbackService.shared.error()
+                Text("deleted")
+                    .font(.footnote)
+                    .foregroundColor(.gray)
+                    .padding(.top, 6)
+            }
+        }
+        
+        @ViewBuilder
+        var mainView: some View {
+            VStack(alignment: .leading, spacing: 0) {
+                VStack(spacing: 0) {
+                    nameRow.padding(.bottom, 4)
+                    if isCollapsed {
+                        Button {
+                            HapticFeedbackService.shared.ultralight()
+                            withAnimation {
+                                isCollapsed.toggle()
+                            }
+                        } label: {
+                            Text("Collapsed")
+                                .font(.footnote.weight(.bold))
+                                .foregroundColor(getColor(level: level))
+                        }
+                        
+                    } else {
+                        textView
+                            .toast(isPresenting: $showFlagToast) {
+                                AlertToast(type: .regular, title: "Flagged")
+                            }
+                            .toast(isPresenting: $showUpvoteToast) {
+                                AlertToast(type: .regular, title: "Upvoted")
+                            }
+                            .toast(isPresenting: $showDownvoteToast) {
+                                AlertToast(type: .regular, title: "Downvoted")
+                            }
+                            .toast(isPresenting: $showReplyToast) {
+                                AlertToast(type: .regular, title: "Replied")
+                            }
+                            .toast(isPresenting: $showFavoriteToast) {
+                                AlertToast(type: .regular, title: "Added")
+                            }
+                    }
+                    if itemStore.loadingItem == comment.id {
+                        LoadingIndicator().padding(.top, 16).padding(.bottom, 8)
+                    } else if itemStore.loadedItems.contains(comment.id) == false && isCollapsed == false && comment.kids.isNotNullOrEmpty {
+                        Button {
+                            HapticFeedbackService.shared.light()
+                            
+                            onLoadMore()
+                        } label: {
+                            Text("Load \(comment.kids.countOrZero) \(comment.kids.isMoreThanOne ? "replies" : "reply")")
+                                .font(.footnote.weight(.bold))
+                                .foregroundColor(getColor(level: level))
+                        }
+                        .buttonStyle(.bordered)
+                        .buttonBorderShape(.capsule)
+                        .padding(.top, 6)
+                    }
+                }
+                .padding(EdgeInsets(top: 6, leading: 0, bottom: 0, trailing: 0))
+                .background(Color(UIColor.systemBackground))
+                .contextMenu {
+                    UpvoteButton(id: comment.id, showUpvoteToast: $showUpvoteToast)
+                    DownvoteButton(id: comment.id, showDownvoteToast: $showDownvoteToast)
+                    FavButton(id: comment.id, showUnfavoriteToast: $showUnfavoriteToast, showFavoriteToast: $showFavoriteToast)
+                    PinButton(id: comment.id)
+                    Button {
+                        showReplySheet = true
+                    } label: {
+                        Label("Reply", systemImage: "plus.message")
+                    }
+                    .disabled(!auth.loggedIn)
+                    Divider()
+                    FlagButton(id: comment.id, showFlagDialog: $showFlagDialog)
+                    Divider()
+                    ShareMenu(item: comment)
+                    Button {
+                        showHNSheet = true
+                    } label: {
+                        Label("View on Hacker News", systemImage: "safari")
+                    }
+                }
+                .onTapGesture {
+                    HapticFeedbackService.shared.ultralight()
+                    withAnimation {
+                        isCollapsed.toggle()
+                    }
+                }
+                Spacer()
+            }
+            .frame(alignment: .leading)
+            .padding(.leading, 6)
+        }
+        
+        @ViewBuilder
+        var nameRow: some View {
+            HStack {
+                Text(comment.by.orEmpty)
+                    .borderedFootnote()
+                    .foregroundColor(getColor(level: level))
+                if let karma = comment.score {
+                    Text("\(karma) karma")
+                        .borderedFootnote()
+                        .foregroundColor(getColor(level: level))
+                }
+                if let descendants = comment.descendants {
+                    Text("\(descendants) comment\(descendants <= 1 ? "" : "s")")
+                        .borderedFootnote()
+                        .foregroundColor(getColor(level: level))
+                }
+                Spacer()
+                Text(comment.timeAgo)
+                    .borderedFootnote()
+                    .foregroundColor(getColor(level: level))
+                    .padding(.trailing, 2)
+            }
+        }
+        
+        private func onFlagTap() {
+            Task {
+                let res = await AuthRepository.shared.flag(comment.id)
+                
+                if res {
+                    showFlagToast = true
+                    HapticFeedbackService.shared.success()
+                } else {
+                    HapticFeedbackService.shared.error()
+                }
             }
         }
     }
